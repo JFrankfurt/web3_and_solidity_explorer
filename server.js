4f802e0d4f9d698d19629471202ddc4280e0b0ae
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2018-03-06 00:46:40 -0500

    some production tweaks

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const next = require('next');
 const bodyParser = require('body-parser');
 
 const apiRouter = require('./api/index');
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== 'production' && !process.env.NOW;
 const app = next({dev});
 const handle = app.getRequestHandler();
 
